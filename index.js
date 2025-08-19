72e53bf8db7253b91acdf596276bf865b7a56f28
Levi Hicks <levih@n11.dev>
2025-08-18 19:14:59 -0500

    Fixed SSO
    Signed-off-by: Levi Hicks <levih@n11.dev>

--- index.js
+++ index.js
@@ -44,20 +44,20 @@ app.use(async (req, res, next) => {
   }
   const token = req.cookies.key ? req.cookies.key : req.cookies.token;
   if (!token) {
-    res.redirect(`https://n11.dev/auth?referrer=${process.env.HOST}&href=/auth`);
+    res.redirect(`https://n11.dev/accounts?referrer=${process.env.HOST}&href=/auth`);
     return;
   }
   try {
     const data = await secureUser.getData(token);
     if (data.error) {
       console.log(data.error);
-      res.redirect(`https://n11.dev/auth?referrer=${process.env.HOST}&href=/auth`);
+      res.redirect(`https://n11.dev/accounts?referrer=${process.env.HOST}&href=/auth`);
       return;
     }
     let user = await findUser(data.username);
     if (!user) {
       console.log("User not found");
-      res.redirect(`https://n11.dev/auth?referrer=${process.env.HOST}&href=/auth`);
+      res.redirect(`https://n11.dev/accounts?referrer=${process.env.HOST}&href=/auth`);
       return;
     }
     // set data in req.user
